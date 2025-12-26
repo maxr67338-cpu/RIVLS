da561ffdd55dca7e52ea5b54a497b75c134ba6de
maxr67338-cpu <maxr67338@gmail.com>
2025-12-26 17:54:42 +0100

    main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- KEY SYSTEM (Linkvertise → Key → Unlock)
-local KEY = "WER-WIE-WO-DAS"
+local KEY = "SAMMY DER HUND"
 local LINKVERTISE = "https://linkvertise.com/1455975/HPLegBGBXSoJ?o=sharing"
 
 -- Copy link to clipboard
